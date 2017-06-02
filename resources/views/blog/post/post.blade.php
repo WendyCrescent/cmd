41ac8011170043193b7d2e2bb8656a11757bb9ff
Wendy Crescent <wendy@wendy-crescent.com>
2017-06-02 16:40:48 +0200

    removed comment section

--- resources/views/blog/post/post.blade.php
+++ resources/views/blog/post/post.blade.php
@@ -36,25 +36,4 @@
   </article>
 </div>
 
-<div class="row hidden">
-  <div class="col-md-8 col-md-offset-2">
-    <div class="well">
-      <h4>Leave a Comment:</h4>
-      <form role="form">
-          <div class="form-group">
-              <textarea class="form-control" rows="3"></textarea>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
-
-    <hr>
-
-    @if($comments)
-      @include('post.partials.comment', ['comments' => $comments])
-    @endif
-
-  </div>
-</div>
-
 @endsection
